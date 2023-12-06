9ca1d83913abbe997996f40f9195a622b3f8dec5
Majalah_Kusam <75724136+mrscriptword@users.noreply.github.com>
2023-12-06 04:37:23 +0000

    Nambahin Struct

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct login{
+string username;
+string password;
+};
+
+struct profile{
+string nama;
+string status;
+string bank;
+string catatan;
+};
+
+struct code1{
+long long int saldo;
+long long int jumlah_saldo;
+};
