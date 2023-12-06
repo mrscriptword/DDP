5ae2ebb5dcc157e56f9502929c923c7a87650d12
ramadhafidz <hrghiffari@gmail.com>
2023-12-06 11:42:35 +0700

    Rapihin aja + bikin main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 
 using namespace std;
 
-struct login{
-string username;
-string password;
+struct login {
+  string username;
+  string password;
 };
 
-struct profile{
-string nama;
-string status;
-string bank;
-string catatan;
+struct profile {
+  string nama;
+  string status;
+  string bank;
+  string catatan;
 };
 
-struct code1{
-long long int saldo;
-long long int jumlah_saldo;
+struct saldo {
+  long long int saldo;
+  long long int jumlah_saldo;
 };
+
+int main() {
+  
+}
