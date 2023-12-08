5efaf557c4410474bf8f1151f9e0ec30a12df9f5
Majalah_Kusam <75724136+mrscriptword@users.noreply.github.com>
2023-12-08 21:33:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,88 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-struct login {
-  string username;
-  string password;
+struct User{
+string username;
+string password;
 };
 
-struct profile {
-  string nama;
-  string status;
-  string bank;
-  string catatan;
+struct profile{
+string nama;
+string status;
+string bank;
+string catatan;
 };
 
-struct saldo {
-  long long int saldo;
-  long long int jumlah_saldo;
+struct code1{
+long long int saldo;
+long long int jumlah_saldo;
 };
 
-int main() {
+bool signup(const User &pengguna){
+ofstream database("Data akun.txt", ios::app);
+if (!database.is_open()){
+    cerr << "Maaf sepertinya ada operasi yang error" << endl;
+    return false;
+}
+database << pengguna.username << " " << pengguna.password << endl;
+database.close();
+cout << "Selamat akun anda telah berhasil dibuat" << endl;
+return true;
+}
 
+bool login(const User &pengguna){
+ifstream database ("Data akun.txt");
+string db_username,db_password;
+if (!database.is_open()){
+    cerr << "Maaf Sepertinya ada yang salah" << endl;
+    return false;
+}
+User db_login;
+while (database >> db_login.username >> db_login.password){
+    if(pengguna.username == db_login.username && pengguna.password == db_login.password){
+        database.close();
+        cout << "Anda telah berhasil login"<< endl;
+        return true;
+    }
+}
+database.close();
+return false;
+}
+
+void daftar(){
+User pengguna;
+cout << "Masukan Username :";
+cin >> pengguna.username;
+cout << "Masukan Password :";
+cin >> pengguna.password;
+signup((pengguna));
+}
+
+void masuk(){
+User pengguna;
+cout << "Username :";
+cin >> pengguna.username;
+cout << "Password :";
+cin >> pengguna.password;
+login((pengguna));
+}
+
+int main(){
+int menu;
+do {
+    cout << "1.Daftar Akun" << endl;
+    cout << "2.Login" << endl;
+    cout << "Pilih Menu :";
+    cin >> menu;
+    if (menu == 1){
+        daftar();
+        cout << "Silahkan Login" << endl;
+        cout << endl;
+    } else if (menu == 2){
+        masuk();
+    }
+} while(menu != 7);
 }
