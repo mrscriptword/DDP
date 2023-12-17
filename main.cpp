f26d0b5ba71c161dcee0ae223b07a0d30f425027
Majalah_Kusam <75724136+mrscriptword@users.noreply.github.com>
2023-12-18 00:17:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,80 +15,46 @@ struct Profile {
     string bank;
     string catatan;
     string usia;
+    string line;
     struct Profile *next;
 };
+
 
 bool login(const User &pengguna) {
     ifstream database("Data akun.txt");
     string db_username, db_password;
+
     if (!database.is_open()) {
         cerr << "Maaf Sepertinya ada yang salah" << endl;
         return false;
     }
+
     User db_login;
     while (database >> db_login.username >> db_login.password) {
         if (pengguna.username == db_login.username && pengguna.password == db_login.password) {
             database.close();
             cout << "Anda telah berhasil login" << endl;
+            cout << db_login.username << endl;
             cout << "-------------------------" << endl;
             cout << endl;
             return true;
-        } else {
-         cout << "Sepertinya anda memasukan sandi atau username yang salah" << endl;
-         cout << "--------------------------------------------------------" << endl;
-         cout << endl;
-         return false;
         }
     }
+
     database.close();
-    return false;
-}
-bool inisiasi = false;
-
-void tambahprf(Profile **head){
-    Profile *tambah = new Profile();
-    cout << "Tambahkan Nama :"; cin.ignore(); getline(cin,tambah->nama);
-    cout << "Tambahkan Status (Pekerja/Pelajar)"; getline(cin,tambah->status);
-    cout << "Tambahkan Usia :"; getline(cin,tambah->usia);
-    tambah->next = NULL;
-    if(*head == NULL){
-        (*head) = tambah;
-        return;
-    } Profile *temp = (*head);
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
-    temp->next = tambah;
-}
-
-void instalasi(){
-    struct Profile *HEAD = NULL;
-if (!inisiasi){
-    tambahprf(&HEAD);
-    inisiasi = true;
-} else {
-cout << " " << endl;
-}
-}
-void profil(Profile *head){
-    Profile *temp = head;
-    if (temp==NULL){
-        cout << "Tidak ada Profile,silahkan tambahkan terlebih dahulu....." << endl;
-        return;
-    }while(temp != NULL){
-    cout << "Nama: " << temp->nama << endl;
-    cout << "Usia: " << temp->usia << endl;
-    cout << "Status: " << temp->status << endl;
-    break;
-    }
+    cout << "Sepertinya anda memasukan sandi atau username yang salah" << endl;
+    cout << "--------------------------------------------------------" << endl;
     cout << endl;
+    return false;
 }
 bool signup(const User &pengguna) {
     ofstream database("Data akun.txt", ios::app);
+
     if (!database.is_open()) {
         cerr << "Maaf sepertinya ada operasi yang error" << endl;
         return false;
     }
+
     database << pengguna.username << " " << pengguna.password << endl;
     database.close();
     cout << "Selamat akun anda telah berhasil dibuat" << endl;
@@ -97,44 +63,76 @@ bool signup(const User &pengguna) {
     cout << "Silahkan Tambahkan Profil........." << endl;
     return true;
 }
-void daftar() {
-    User pengguna;
-    cout << "Masuan Username: ";
-    cin >> pengguna.username;
-    cout << "Masukan Password: ";
-    cin >> pengguna.password;
-    signup(pengguna);
-    instalasi();
-}
+
 void masuk() {
     User pengguna;
-    cout << "Username: ";
-    cin >> pengguna.username;
-    cout << "Password: ";
-    cin >> pengguna.password;
-    login(pengguna);
+    cout << "Masukan Username: ";
+    cin.ignore(); getline(cin, pengguna.username);
+    cout << "Masukan Password: ";
+    getline(cin, pengguna.password);
+    if (login(pengguna)){
+            int a, menu;
+            a = 1;
+            do {
+            cout << "SELAMAT ANDA BERHASIL MASUK" << endl;
+            cout << "SILAHKAN PILIH MENU YANG TERSEDIA" << endl;
+            cout << "------------------------------------" << endl;
+            cout << "1.MENU ADA" << endl;
+            cout << "2.MENU ADA" << endl;
+            cout << "1.MENU ADA" << endl;
+            cout << "2.MENU ADA" << endl;
+            } while (a > 0);
+    }
 }
+
+void daftar(Profile &profil) {
+    User pengguna;
+    cout << "Masukan Username: ";
+    cin.ignore(); getline(cin, pengguna.username);
+    cout << "Masukan Password: ";
+    getline(cin, pengguna.password);
+
+    if (signup(pengguna)) {
+            int a, menu;
+            a = 1;
+            do {
+            cout << "SELAMAT ANDA BERHASIL MASUK" << endl;
+            cout << "SILAHKAN PILIH MENU YANG TERSEDIA" << endl;
+            cout << "------------------------------------" << endl;
+            cout << "1.MENU ADA" << endl;
+            cout << "2.MENU ADA" << endl;
+            cout << "1.MENU ADA" << endl;
+            cout << "2.MENU ADA" << endl;
+            } while (a > 0);
+    }
+    
+}
+
+
+
 
 
 int main() {
     int menu;
-    struct Profile *HEAD = NULL;
+    Profile profil;
+
     do {
         cout << "1. Daftar Akun" << endl;
         cout << "2. Login" << endl;
         cout << "3. Profil" << endl;
         cout << "Pilih Menu: ";
         cin >> menu;
+
         if (menu == 1) {
-            daftar();
+            daftar(profil);  // Pass the profil object
             cout << "Silahkan Login" << endl;
             cout << endl;
         } else if (menu == 2) {
             masuk();
         } else if (menu == 3) {
-            profil(HEAD);
-        } else if (menu == 4){
+            cout << endl;
         }
+
     } while (menu != 7);
 
     return 0;
